fabda6048166d4622696f08c5494107e7df59d23
Shraddha Borade <Shraddha.Borade@mauli-infotech.co.in>
2023-04-17 12:17:12 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from "./Redux/store"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Auth0Provider domain="dev-s0ldrvwo1tpjbpsr.us.auth0.com"
+      clientId="WioGJXF7JTnGXVRUqz7763czRc1lYEeQ"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }} >
+      <ChakraProvider>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ChakraProvider>
+    </Auth0Provider>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
